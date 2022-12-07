5a3a07eef2fd0ec651d273d6376f17d45364b45c
Aleksei Noskov <aleksei_noskov@epam.com>
2022-12-07 18:03:55 +0100

    implemented env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = "RSS_";
+
+    const keysWithPrefix = Object.keys(process.env)
+        .filter(key => key.startsWith(prefix));
+    const messageForConsole = keysWithPrefix
+        .map(key => `${key}=${process.env[key]}`)
+        .join("; ");
+
+    console.log(messageForConsole);
 };
 
 parseEnv();
