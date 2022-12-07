6afd4580d60cbf3bb10ca55b5d2f41d00a083808
Aleksei Noskov <aleksei_noskov@epam.com>
2022-12-07 12:33:09 +0100

    copy & create

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import fs from 'node:fs/promises';
+import { join } from 'node:path';
+
 const copy = async () => {
-    // Write your code here 
+    const sourceFolder = "./files";
+    const destinationFolder = "./files_copy";
+
+    //todo: if dst folder does exists, error and exit 
+
+    // I assume that we also copy contents of nested folders? Implemented this way.
+    const copyFolderWithContents = async (sourceFolder, destinationFolder) => {
+        await fs.mkdir(destinationFolder);
+        const filesAndFolders = await fs.readdir(sourceFolder);
+        for (const fileOrFolder of filesAndFolders) {
+            const srcName = join(sourceFolder, fileOrFolder);
+            const destName = join(destinationFolder, fileOrFolder);
+            const isDirectory = await (await fs.stat(srcName)).isDirectory();
+            if (isDirectory) {
+                copyFolderWithContents(srcName, destName);
+            } else {
+                fs.copyFile(srcName, destName);
+            }
+        }
+    }
+
+    await copyFolderWithContents(sourceFolder, destinationFolder);
 };
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'node:fs/promises';
+import { stdout } from 'node:process';
+
 const create = async () => {
-    // Write your code here 
+    const contentToSave = "I am fresh and young";
+    const filePath = "./files/fresh.txt";
+
+    stdout.write(process.cwd());
+
+    let isFileExists;
+    try {
+        await fs.access(filePath, fs.constants.F_OK);
+        isFileExists = true;
+    }
+    catch { }
+    
+    if (isFileExists) {
+        throw new Error("FS operation failed");
+    }
+    
+    await fs.writeFile(filePath, contentToSave);
 };
 
 await create();
