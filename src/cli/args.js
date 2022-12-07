85263d0f788b442e77a001ac7875e7cce0e32525
Aleksei Noskov <aleksei_noskov@epam.com>
2022-12-07 18:25:58 +0100

    implemented args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argPrefix = "--";
+
+    const argsPairsUnparsed = process.argv
+        .filter(key => key.startsWith(argPrefix));
+
+    const argsParsed = argsPairsUnparsed
+        .map(unparsedArg => {
+            const withoutPrefix = unparsedArg.slice(2);
+            const keyAndValSplitted = withoutPrefix.split("=");
+            return { key: keyAndValSplitted[0], val: keyAndValSplitted[1] };
+        })
+
+    const messageForConsole = argsParsed
+        .map(arg => `${arg.key} is ${arg.val}`)
+        .join(", ");
+
+    console.log(messageForConsole);
 };
 
 parseArgs();
