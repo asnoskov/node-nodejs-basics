176d57af10f5d9f234295cca0c894713877bc204
Aleksei Noskov <aleksei_noskov@epam.com>
2022-12-11 18:46:20 +0100

    workers implemented

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { Worker } from "worker_threads";
+import os from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+    const threadCount = os.cpus().length;
+    const workerFileName = "./worker.js";
+
+    const results = [];
+    const threads = new Set();
+    for (let n = 0; n < threadCount; n++) {
+        threads.add(new Worker(workerFileName, { workerData: { n } }));
+    }
+    for (let worker of threads) {
+        worker.on('error', (err) => { throw err; });
+        worker.on('message', (msg) => {
+            results.push(msg);
+            threads.delete(worker);
+            if (threads.size == 0) {
+                console.log(results.map(r => JSON.stringify(r)).join('\n'));
+            }
+        });
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,24 @@
+import { parentPort, workerData } from "worker_threads";
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    let res;
+    try {
+        const fibonacci = nthFibonacci(workerData.n);
+        res = {
+            status: 'resolved',
+            data: fibonacci
+        }
+    }
+    catch {
+        res = {
+            status: 'error'
+        }
+    }
+
+    parentPort.postMessage(res);
 };
 
 sendResult();
