b2c4c548b351a242a5dda4c9bf289c34fb2b0bba
Aleksei Noskov <aleksei_noskov@epam.com>
2022-12-11 16:38:30 +0100

    compress & decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const compress = async () => {
-    // Write your code here 
+    const sourcePath = "./files/fileToCompress.txt";
+    const destinationPath = "./files/archive.gz";
+    const gzip = createGzip();
+    const source = createReadStream(sourcePath);
+    const destination = createWriteStream(destinationPath);
+
+    await pipeline(source, gzip, destination);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const decompress = async () => {
-    // Write your code here 
+    const sourcePath = "./files/archive.gz";
+    const destinationPath = "./files/fileToCompress.txt";
+    const gUnzip = createGunzip();
+    const source = createReadStream(sourcePath);
+    const destination = createWriteStream(destinationPath);
+
+    await pipeline(source, gUnzip, destination);
 };
 
 await decompress();
