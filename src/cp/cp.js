d83895e10fcc3b561e66abf0cb51d1dfb9d431a1
Aleksei Noskov <aleksei_noskov@epam.com>
2022-12-11 20:39:41 +0100

    cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,9 @@
+import { fork } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const fileName = "./files/script.js";
+
+    const myChildProcess = fork(fileName, process.argv);
 };
 
 spawnChildProcess();
